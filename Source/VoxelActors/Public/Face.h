20815b79484dc1a835974c880bb15db92a56e2a0
Kyle Roarty <kyleroarty1716@gmail.com>
2019-04-19 16:42:54 -0500

    Add Face class
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

---
+++ Source/VoxelActors/Public/Face.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class VOXELACTORS_API Face
+{
+public:
+	Face();
+	~Face();
+};
