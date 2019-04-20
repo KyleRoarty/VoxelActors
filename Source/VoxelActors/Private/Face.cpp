2342b39b3f3004af097ad8045d58a34f7d62fba0
Kyle Roarty <kyleroarty1716@gmail.com>
2019-04-19 20:17:41 -0500

    Face: Getters/Setters, Constructor
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- Source/VoxelActors/Private/Face.cpp
+++ Source/VoxelActors/Private/Face.cpp
@@ -2,10 +2,67 @@
 
 #include "Face.h"
 
-Face::Face()
+Face::Face(TArray<FVector> points, FVector offset)
 {
+	this->furthest = FVector::ZeroVector;
+	for (FVector p : points)
+		if (p.Size() > this->furthest.Size())
+			this->furthest = p;
+
+	this->points = points;
+	this->points.Sort([this](const FVector A, const FVector B) {
+		return (A - furthest).Size() < (B - furthest).Size();
+	});
+
+	this->offset = offset;
+	GenerateTris();
 }
 
 Face::~Face()
+{
+}
+
+TArray<FVector> Face::GetNormals()
+{
+	return normals;
+}
+
+void Face::SetNormals(TArray<FVector> normals)
+{
+	this->normals = normals;
+}
+
+TArray<FProcMeshTangent> Face::GetTangents()
+{
+	return tangents;
+}
+
+void Face::SetTangents(TArray<FProcMeshTangent> tangents)
+{
+	this->tangents = tangents;
+}
+
+TArray<FVector2D> Face::GetUVs()
+{
+	return uvs;
+}
+
+void Face::SetUVs(TArray<FVector2D> uvs)
+{
+	this->uvs = uvs;
+}
+
+TArray<FLinearColor> Face::GetColors()
+{
+	return colors;
+}
+
+void Face::SetColors(TArray<FLinearColor> colors)
+{
+	this->colors = colors;
+}
+
+// Internal function
+void Face::GenerateTris()
 {
 }
--- Source/VoxelActors/Public/Face.h
+++ Source/VoxelActors/Public/Face.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "ProceduralMeshComponent.h"
+#include "KismetProceduralMeshLibrary.h"
 
 /**
  * 
@@ -10,6 +12,33 @@
 class VOXELACTORS_API Face
 {
 public:
-	Face();
+	Face(TArray<FVector> points, FVector offset);
 	~Face();
+
+
+	TArray<FVector> GetNormals();
+	void SetNormals(TArray<FVector> normals);
+
+	TArray<FProcMeshTangent> GetTangents();
+	void SetTangents(TArray<FProcMeshTangent> tangents);
+
+	TArray<FVector2D> GetUVs();
+	void SetUVs(TArray<FVector2D> uvs);
+
+	TArray<FLinearColor> GetColors();
+	void SetColors(TArray<FLinearColor> colors);
+
+private:
+	TArray<FVector> points;
+	FVector offset;
+	FVector furthest;
+
+	// Stuff relating to creating the face as a procedural generated plane
+	TArray<int32> pt_trip; // Triplet of points that make a triangle
+	TArray<FVector> normals;
+	TArray<FProcMeshTangent> tangents;
+	TArray<FVector2D> uvs;
+	TArray<FLinearColor> colors;
+
+	void GenerateTris();
 };
